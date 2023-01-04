44faea6cb40f5b7859c7ba12eef82764add4e6d1
Jungsik Won <wl808@naver.com>
2023-01-04 21:18:56 +0900

    0104 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        Integer a2 = 10;
+        Integer b2 = 20;
 
+        System.out.println(Objects.hashCode(a));
+        System.out.println(Objects.hashCode(b));
+        System.out.println(a2.hashCode());
+        System.out.println(b2.hashCode());
     }
 }
